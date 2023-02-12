f2d0cefe1bbd60a80228bacd31861fa1a1a7f9e7
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-02-12 17:34:12 +0000

    Added info comments

--- app/addContract.js
+++ app/addContract.js
@@ -1,26 +1,32 @@
-import {ethers} from 'ethers';
+import { ethers } from 'ethers';
 
 const provider = new ethers.providers.Web3Provider(ethereum);
 
 export default async function addContract(id, contract, arbiter, beneficiary, value) {
-  const buttonId = `approve-${id}`;
+	// Create a button ID
+	const buttonId = `approve-${id}`;
 
-  const container = document.getElementById("container");
-  container.innerHTML += createHTML(buttonId, arbiter, beneficiary, value);
+	// Create the HTML
+	const container = document.getElementById('container');
+	container.innerHTML += createHTML(buttonId, arbiter, beneficiary, value);
 
-  contract.on('Approved', () => {
-    document.getElementById(buttonId).className = "complete";
-    document.getElementById(buttonId).innerText = "✓ It's been approved!";
-  });
+	// Listen for the approval event and update the button
+	contract.on('Approved', () => {
+		document.getElementById(buttonId).className = 'complete';
+		document.getElementById(buttonId).innerText = "✓ It's been approved!";
+	});
 
-  document.getElementById(buttonId).addEventListener("click", async () => {
-    const signer = provider.getSigner();
-    await contract.connect(signer).approve();
-  });
+	// Listen for the approval event and update the button
+	document.getElementById(buttonId).addEventListener('click', async () => {
+		const signer = provider.getSigner();
+		await contract.connect(signer).approve();
+	});
 }
 
+// Path: app/createContract.js
+// create html structure for new contract
 function createHTML(buttonId, arbiter, beneficiary, value) {
-  return `
+	return `
     <div class="existing-contract">
       <ul className="fields">
         <li>
